6bc1e4587cbb8247657088aeed688fefea5ab14a
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-02-13 14:52:47 +0000

    refactor: adding staking verification params

--- scripts/audit_staking_setups/audit_setup.js
+++ scripts/audit_staking_setups/audit_setup.js
@@ -3,7 +3,23 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 const fs = require("fs");
-const readline = require('readline');
+const readline = require("readline");
+
+// Custom expect that is wrapped into try / catch block
+function customExpect(arg1, arg2, log) {
+    try {
+        expect(arg1).to.equal(arg2);
+    } catch (error) {
+        console.log(log);
+        if (error.status) {
+            console.error(error.status);
+            console.log("\n");
+        } else {
+            console.error(error);
+            console.log("\n");
+        }
+    }
+}
 
 async function main() {
     // TODO - take from tokenomics contract on L1
@@ -39,8 +55,46 @@ async function main() {
             const wallet = new ethers.Wallet(account, provider);
             const stakingToken = await ethers.getContractAt("StakingToken", stakingTokenAddress, wallet);
 
-            const activityChecker = await stakingToken.activityChecker();
-            expect(activityChecker).to.equal(params["stakingParams"]["activityChecker"]);
+            const log = "Contract " + stakingTokenAddress + ", chain: " + params["providerName"]
+            const metadataHash = await stakingToken.metadataHash();
+            customExpect(metadataHash, params["stakingParams"]["metadataHash"], log + ", metadataHash");
+            const maxNumServices = await stakingToken.maxNumServices();
+            customExpect(maxNumServices, params["stakingParams"]["maxNumServices"], log + ", maxNumServices");
+            const rewardsPerSecond = await stakingToken.rewardsPerSecond();
+            customExpect(rewardsPerSecond, params["stakingParams"]["rewardsPerSecond"], log + ", rewardsPerSecond");
+            const minStakingDeposit = await stakingToken.minStakingDeposit();
+            customExpect(minStakingDeposit, params["stakingParams"]["minStakingDeposit"], log + ", minStakingDeposit");
+
+            const livenessPeriod = await stakingToken.livenessPeriod();
+            customExpect(livenessPeriod, params["stakingParams"]["livenessPeriod"], log + ", livenessPeriod");
+            const minNumStakingPeriods = params["stakingParams"]["minNumStakingPeriods"];
+            const minStakingDuration = await stakingToken.minStakingDuration();
+            customExpect(minStakingDuration, minNumStakingPeriods * livenessPeriod, log + ", minStakingDuration");
+            const maxNumInactivityPeriods = params["stakingParams"]["maxNumInactivityPeriods"];
+            const maxInactivityDuration = await stakingToken.maxInactivityDuration();
+            customExpect(maxInactivityDuration, maxNumInactivityPeriods * livenessPeriod, log + ", maxInactivityDuration");
+
+
+            const timeForEmissions = await stakingToken.timeForEmissions();
+            customExpect(timeForEmissions, params["stakingParams"]["timeForEmissions"], log + ", timeForEmissions");
+            const numAgentInstances = await stakingToken.numAgentInstances();
+            customExpect(numAgentInstances, params["stakingParams"]["numAgentInstances"], log + ", numAgentInstances");
+            const agentId = await stakingToken.agentIds(0);
+            customExpect(agentId, params["stakingParams"]["agentIds"][0], log + ", agentIds");
+            const threshold = await stakingToken.threshold();
+            customExpect(threshold, params["stakingParams"]["threshold"], log + ", threshold");
+            const configHash = await stakingToken.configHash();
+            customExpect(configHash, params["stakingParams"]["configHash"], log + ", configHash");
+            const proxyHash = await stakingToken.proxyHash();
+            customExpect(proxyHash, params["stakingParams"]["proxyHash"], log + ", proxyHash");
+            const serviceRegistry = await stakingToken.serviceRegistry();
+            customExpect(serviceRegistry, params["stakingParams"]["serviceRegistry"], log + ", serviceRegistry");
+
+            const activityCheckerAddress = await stakingToken.activityChecker();
+            customExpect(activityCheckerAddress, params["stakingParams"]["activityChecker"], log + ", activityChecker");
+            const activityChecker = await ethers.getContractAt("RequesterActivityChecker", activityCheckerAddress, wallet);
+            const livenessRatio = await activityChecker.livenessRatio();
+            customExpect(livenessRatio, params["livenessRatio"], log + ", livenessRatio");
         }
     }
 }
