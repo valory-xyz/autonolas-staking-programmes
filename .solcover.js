580c06391fd11714f25370d49b7aca1c247bb4b9
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-08-18 18:58:23 +0100

    feat: Quorum staking token activity checker concept

--- .solcover.js
+++ .solcover.js
@@ -3,10 +3,10 @@ module.exports = {
         "contribute/deprecated/ContributeManager.sol",
         "contribute/deprecated/ContributorsDeprecated.sol",
         "mech_usage/deprecated/SingleMechActivityChecker.sol",
-        "contracts/dual_token/test/MockEAS.sol",
         "test/ABICreator.sol",
         "test/ERC20Token.sol",
         "test/MockAgentMech.sol",
+        "test/MockEAS.sol",
         "test/SafeNonceLib.sol"
     ]
 };
